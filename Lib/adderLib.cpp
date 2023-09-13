8822f84dd181faf39627e728a87e33348ea6a0be
amire <amir.h.ebrahimnezhad@gmail.com>
2023-09-13 12:15:34 -0700

    add ambiguity

--- Lib/adderLib.cpp
+++ Lib/adderLib.cpp
@@ -1,11 +1,24 @@
 #include "adderLib.h"
 
-int mearlyLib::add(int a, int b)
-{
-    return(a+b);
+namespace mearlyLib {
+    int add(int a, int b) {
+        return a + b;
+    }
+
+    float add(float a, float b) {
+        return a + b;
+    }
+
+    double add(double a, double b) {
+        return a + b;
+    }
+
+    float add(int a, float b) {
+        return static_cast<float>(a) + b;
+    }
+
+    float add(float a, int b) {
+        return a + static_cast<float>(b);
+    }
 }
 
-float mearlyLib::add(float a, float b)
-{
-    return(a+b);
-}
--- Lib/adderLib.h
+++ Lib/adderLib.h
@@ -1,7 +1,12 @@
-namespace mearlyLib 
-{
+#ifndef ADDERLIB_H
+#define ADDERLIB_H
+
+namespace mearlyLib {
     int add(int a, int b);
     float add(float a, float b);
-
+    double add(double a, double b);
+    float add(int a, float b);
+    float add(float a, int b);
 }
 
+#endif // ADDERLIB_H
